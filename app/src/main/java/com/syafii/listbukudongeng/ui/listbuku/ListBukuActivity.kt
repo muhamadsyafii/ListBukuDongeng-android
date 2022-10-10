25859dcf75b118ab82411b97a45538029bbbacaa
Muhamad Syafii <muhamad.syafii@moladin.com>
2022-10-10 21:27:27 +0700

    add some picture, add lisbuku activity, add styles

---
+++ app/src/main/java/com/syafii/listbukudongeng/ui/listbuku/ListBukuActivity.kt
@@ -0,0 +1,13 @@
+package com.syafii.listbukudongeng.ui.listbuku
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.syafii.listbukudongeng.R
+
+class ListBukuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_buku)
+
+    }
+}
